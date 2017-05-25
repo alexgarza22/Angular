937fa5569bac0a504664366a411f45ed9636e114
alexhor22 <alexhor22@gmail.com>
2017-05-25 10:46:22 -0500

    Finished the code
    finished the toDo list

--- ToDoList/controller.js
+++ ToDoList/controller.js
@@ -1,4 +1,27 @@
 angular.module("ToDoList",["LocalStorageModule"])
-.controller("ToDoController", function($scope,LocalStorageService){
+.controller("ToDoController", function($scope, localStorageService){
+	if (localStorageService.get("ATDL")){
+		$scope.todo = localStorageService.get("ATDL");
+	}
+	else{
+		$scope.todo = [];
+	}
+	
+	$scope.newAct = {};
 
+	$scope.$watchCollection("todo", function(nVal,oldVal)
+		{
+			localStorageService.set("ATDL",$scope.todo);
+		});
+	$scope.addAcvtivity = function()
+	{
+		$scope.todo.push($scope.newAct);
+		$scope.newAct = {};
+		
+	}
+	$scope.despejar = function()
+	{
+		$scope.todo = [];
+		
+	}
 })
